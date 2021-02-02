4094bb52d48f704f029c4ba9bc1843fb3114736e
Oleh Tsoh <olehtsoh@gmail.com>
2021-02-02 12:22:47 +0200

    Added two new files with homework #1

---
+++ HW_1/HW_1.2.py
@@ -0,0 +1,11 @@
+a = int(input("Set number a"))
+b = int(input("Set number b"))
+
+print(a + b)
+print(a - b)
+print(a * b)
+if b == 0:
+    print("Can't divide by zero")
+else:
+    print(a / b)
+print(a**b)
