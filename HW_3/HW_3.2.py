07dc114191d1e24ef9262d81b10d36651a69219a
Oleh Tsoh <olehtsoh@gmail.com>
2021-02-03 12:55:52 +0200

    homework #3, 3 files

---
+++ HW_3/HW_3.2.py
@@ -0,0 +1,10 @@
+x = 5371
+str_x = str(x)
+
+print(int(str_x[0])*int(str_x[1])*int(str_x[2])*int(str_x[3]))
+
+print(str_x[::-1])
+
+str_sorted = sorted(str_x)
+
+print(str_sorted[0] + str_sorted[1] + str_sorted[2] + str_sorted[3])
