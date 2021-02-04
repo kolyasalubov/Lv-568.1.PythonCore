c872b41c6681bae0134b30f22c98fc11ae51b57b
Taras Pyniv <taring888@gmail.com>
2021-02-04 22:59:48 +0200

    add HW_1: task1 and task2

---
+++ HW_1/Taras/Home_Work1_Task_1.py
@@ -0,0 +1,7 @@
+name = input('What your name?: ')
+age = input('How old are you?: ')
+city = input('Where do you live?: ')
+
+print('Hello, ' + name)
+print('Your age is ' + str(age))
+print('You live in ' + city)
