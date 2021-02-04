7793c8d556e7d05814997e61c3f5c1e92f156705
69MiXFiX69 <sviatoslav6666@gmail.com>
2021-02-04 18:21:28 +0200

    added homework

---
+++ LV-568.1 homework/Sviatoslav_Vatamanyuk/Home_work4/l4. t2. odd&even numbers.py
@@ -0,0 +1,26 @@
+# for i in range(100):
+#     if i % 2 == 0:
+#         print(i)
+#     else:
+#         continue
+
+
+#
+# i = 0
+# while i <= 100:
+#     if i % 2 == 0:
+#         print(i)
+#
+#     else:
+#         continue
+#     i += 1
+
+
+# x = -100
+# i = 1
+# while i > x:
+#     i -= 1
+#     if i % 2 == 0:
+#         print(i)
+#     else:
+#         continue
