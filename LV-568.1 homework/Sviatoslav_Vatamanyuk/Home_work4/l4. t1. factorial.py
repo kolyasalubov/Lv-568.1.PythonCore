c37e4921ed36470a50057f29a2a4d90a293053cd
69MiXFiX69 <sviatoslav6666@gmail.com>
2021-02-11 02:59:18 +0200

    adding new homeword and codewars stuff

--- LV-568.1 homework/Sviatoslav_Vatamanyuk/Home_work4/l4. t1. factorial.py
+++
@@ -1,8 +0,0 @@
-x = int(input("Enter your num "))
-factorial = 1
-if x <= 0:
-    print(" You can not use negative numbers or 0")
-else:
-    for i in range(1, x+1):
-        factorial = factorial*i
-        print(factorial)
