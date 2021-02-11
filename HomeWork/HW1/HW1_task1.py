b5f95624e7000a9e0faf65203e375549a6b7e222
Lezhenkin Stanislav <claymore1812@gmail.com>
2021-02-11 15:40:22 +0200

    HomeWork

---
+++ HomeWork/HW1/HW1_task1.py
@@ -0,0 +1,4 @@
+a = int(input('enter random  number for a :'))
+b = int (input ('enter random  number for b : '))
+
+print ('a-b=',a-b,'a+b=',a+b,'a*b=',a*b,'a/b=',a/b,'a**b=',a**b)
---
+++ HomeWork/HW4/HW4_task2.py
@@ -0,0 +1,6 @@
+list_of_number = [11, 5, 84, 9, 1990]
+float_number = []
+
+for f in list_of_number:
+    float_number.append(float(f))
+print(float_number)
