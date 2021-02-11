4fb25b381cb74345a381221899fbcef1923bdc05
Lezhenkin Stanislav <claymore1812@gmail.com>
2021-02-11 18:37:09 +0200

    HW

--- HomeWork/HW5/HW5_task1.py
+++ HomeWork/HW5/HW5_task1.py
@@ -1,4 +1,3 @@
-  
 divisible_by_2 = []
 divisible_by_3 = []
 other_numbers = []
@@ -6,11 +5,11 @@ other_numbers = []
 for x in range(1, 11):
     if x % 3 == 0:
         divisible_by_3.append(x)
-    elif x % 2 == 0:
+    if x % 2 == 0:
         divisible_by_2.append(x)
     else:
         other_numbers.append(x)
 
 print("numbers divisible by 2:",divisible_by_2)
-print("numbers divisible by 2:",divisible_by_3)
+print("numbers divisible by 3:",divisible_by_3)
 print("other_numbers:",other_numbers)
