19eee9ec2798dfb2e38d9cdca0d291199667f1e6
Matlakhov Dmitriy <matlakhovdmitriyqa@gmail.com>
2021-02-12 23:45:52 +0200

    HW_5

---
+++ HW_5/Matlakhov/Home_Work5_Task_1.py
@@ -0,0 +1,22 @@
+# ######## Изменение типа данных #############
+# int_lst = [i for i in range(int(input()))]
+# float_lst = [float(x) for x in int_lst]
+# print(float_lst)
+# ##########
+# int_lst = []
+# x = int(input("Вводи значение листа x=",))
+# int_lst.append(x)
+# float_lst = [float(x) for x in int_lst]
+# print(float_lst)
+
+################
+a = []
+i = 0
+while i < 5:
+    x = int(input("Введи целое число списка: "))
+    a.append(x)
+    print(a)
+    i += 1
+print("Целочисленный список", a)
+b = [float(x) for x in a]
+print("Список с плавающим знаком", b)
