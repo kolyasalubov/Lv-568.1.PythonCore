6a6bf6e63dba38fc174797b750e4a7264d97617b
69MiXFiX69 <sviatoslav6666@gmail.com>
2021-02-15 17:38:47 +0200

    adding homework: lesson 7

---
+++ LV-568.1 homework/Sviatoslav_Vatamanyuk/Home_work 7/the_squares.py
@@ -0,0 +1,39 @@
+def square_circle(r):
+    """
+    function = square_circle
+    :param r:
+    :return: P
+    """
+    P = 2 * 3.14 * r
+    return "The square of circle is ", P
+
+
+def square_triangle(argA, argB, argC):
+    """
+    func is square_triangle
+    :param argA:
+    :param argB:
+    :param argC:
+    return: P
+    """
+    P = argA + argB + argC
+    return "The square of triangle is ", P
+
+
+def square_rectangle(argA, argB):
+    """
+    func is sqare_rectangle
+    :param argA:
+    :param argB:
+    :return: P of rectangle
+    """
+    return "The square of rectangle is ", (argA + argB) * 2
+
+
+"""   entering all arguments and calling functions   """
+
+
+print(square_circle(float(input("Enter r of circle "))))
+print(square_triangle(int(input("Enter triangle's side A ")), int(input("Enter triangle's side B ")),
+                      int(input("Enter triangle's side C "))))
+print(square_rectangle(int(input("Enter rectangle's side A ")), int(input("Enter rectangle's side B "))))
