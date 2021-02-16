dd5f6a89fc1ff0627858e83bbc49017617b8f788
Dmytro-Skorobrekha <skorobrexa15@gmail.com>
2021-02-16 15:40:44 +0200

    Codewars

---
+++ codewars/Dmytro/FindTheDistanceBetweenTwoPoints.py
@@ -0,0 +1,3 @@
+def distance(x1, y1, x2, y2):
+    a = ((x2-x1)**2 + (y2-y1)**2) ** .5
+    return round(a, 2)
---
+++ codewars/Dmytro/IsThisMyTail.py
@@ -0,0 +1,6 @@
+def correct_tail(body, tail):
+    is_teil = body[-1]
+    if is_teil == tail:
+        return True
+    else:
+        return False
