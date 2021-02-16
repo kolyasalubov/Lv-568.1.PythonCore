8ecb8284f0ecd8805e812b4210faf9868e09b91d
69MiXFiX69 <sviatoslav6666@gmail.com>
2021-02-16 13:52:24 +0200

    Adding 3 folder of 'codewars' codes

---
+++ LV-568.1 homework/CodeWars/3/distance.py
@@ -0,0 +1,10 @@
+def distance(x1, y1, x2, y2):
+    """
+    func called distance
+    :param x1:
+    :param y1:
+    :param x2:
+    :param y2:
+    :return: distance
+    """
+    return round((pow((pow(x2 - x1, 2) + pow(y2 - y1, 2) * 1.0), 0.5)), 2)
---
+++ LV-568.1 homework/CodeWars/3/fix_bat.py
@@ -0,0 +1,11 @@
+def create_array(n):
+    res=[]
+    i=1
+    while i<=n:
+        res+=[i]
+        i+=1
+    else:
+        return res
+
+
+print(create_array(2))
