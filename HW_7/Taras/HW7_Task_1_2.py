c10c6cb3d2d7d41d9948ba45489c0cd00c8340fd
Taras Pyniv <taring888@gmail.com>
2021-02-23 00:21:49 +0200

    add HW7: task1

--- HW_7/Taras/HW7_Task_1_2.py
+++ HW_7/Taras/HW7_Task_1_2.py
@@ -12,15 +12,15 @@ if choose == 1:
     height_rectangle = float(input('Enter the height of the rectangle: '))
     width_rectangle = float(input('Enter the width of the rectangle: '))
     print('-' * 10)
-    HW7_Task_2_1.calc_rectangle(height_rectangle, width_rectangle)
+    HW7_Task_1_1.calc_rectangle(height_rectangle, width_rectangle)
 
 elif choose == 2:
     length_triangle = float(input("Enter the length of the side of the triangle: "))
     height_triangle = float(input('Enter the height of the triangle: '))
     print('-' * 10)
-    HW7_Task_2_1.calc_triangle(length_triangle, height_triangle)
+    HW7_Task_1_1.calc_triangle(length_triangle, height_triangle)
 
 elif choose == 3:
     radius = float(input('Enter the radius of the circle: '))
     print('-' * 10)
-    HW7_Task_2_1.calc_circle(radius)
+    HW7_Task_1_1.calc_circle(radius)
