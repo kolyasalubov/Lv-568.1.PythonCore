a45c3a27b80a5cbf51d71f1e19d927127bea823d
Andrii Parashchak <7950649@gmail.com>
2021-03-09 16:39:42 +0200

    jj

--- HW_5/Parashchak_Andrii/Are You Playing Banjo?.py
+++ HW_5/Parashchak_Andrii/Are You Playing Banjo?.py
@@ -1,1 +1,17 @@
-Convert boolean values to strings 'Yes' or 'No'
+def are_you_playing_banjo(name):
+    ''' function name are_you_playing_banjo
+        input parametr: name
+        split the string into characters
+        check if the first character R or r return name + " plays banjo"
+        elso return name + " does not play banjo
+    '''
+    st = list(name)
+    if st[0] == 'R' or st[0] == 'r':
+        return name + " plays banjo"
+    return name + " does not play banjo"
+
+
+a = str(input())
+
+s = are_you_playing_banjo(a)
+print(s)
