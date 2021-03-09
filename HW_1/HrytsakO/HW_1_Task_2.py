43548fa91b4f7c9eb80ec3ecddd7259eb6e665a4
HrytsakO <wildasasnail@gmail.com>
2021-03-09 22:50:51 +0200

    HW_1

---
+++ HW_1/HrytsakO/HW_1_Task_2.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a:"))
+b = int(input("Enter b:"))
+
+print("a + b  = " + str(a + b))
+print("a - b  = " + str(a - b))
+print("a * b  = " + str(a * b))
+print("a % b  = " + str(a % b))
+print("a / b  = " + str(a / b))
+print("a ** b = " + str(a ** b))
